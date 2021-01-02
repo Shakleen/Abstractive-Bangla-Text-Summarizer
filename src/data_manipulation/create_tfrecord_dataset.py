1b8a66a4c280ceb7cafff6043e8e390c71a4d783
Nahid Hasan <nahidhasan43@iut-dhaka.edu>
2021-01-02 11:10:03 +0600

    tfrecord creation description updated

--- src/data_manipulation/create_tfrecord_dataset.py
+++ src/data_manipulation/create_tfrecord_dataset.py
@@ -9,21 +9,23 @@ def create_tfrecord_dataset(
     output_feature_length,   # summary size is limited to 12 words by default
 ):
 
-
+  # Create a source dataset from your content file list.
   dataset = tf.data.TFRecordDataset(filenames = [tfrecord_files])
 
-  # Create a dictionary describing the features.
+  # Create a dictionary of describing the fixed-length features.
   feature_description = {
-      'input': tf.io.FixedLenFeature([input_feature_length], tf.int64),
-      'target': tf.io.FixedLenFeature([output_feature_length], tf.int64),
+      'input': tf.io.FixedLenFeature([input_feature_length], tf.int64),        # one dimensional integer list to represent the example content
+      'target': tf.io.FixedLenFeature([output_feature_length], tf.int64),      # int64 list is expected as the summary output
   }
 
+  # Define a private parsing function to extract 'input', 'target' features from each example
   def _parse(example_proto):
-    example = tf.io.parse_single_example(example_proto, feature_description)
+    example = tf.io.parse_single_example(example_proto, feature_description)   # 
     input, target = example["input"], example["target"]
     return (input, target)
 
+  # transforms _parse across all the elements on dataset
   dataset = dataset.map(_parse)
-  dataset = dataset.cache().shuffle(cache_buffer_size).batch(batch_size)
-  dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)
+  dataset = dataset.cache().shuffle(cache_buffer_size).batch(batch_size)       # caches the dataset to memory in chunks and shuffle the samples
+  dataset = dataset.prefetch(buffer_size=prefetch_buffer_size)                 # setting number of batches to prefetch to improve throughput
   return dataset
